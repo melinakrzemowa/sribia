c1d24e993f2ba28270ba2d92fb9c4f40cd28f7ac
Bartosz Kalinowski <kelostrada@gmail.com>
2023-07-16 14:42:51 +0200

    try to fix alpine version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Abyss.Mixfile do
   def project do
     [
       app: :abyss,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
