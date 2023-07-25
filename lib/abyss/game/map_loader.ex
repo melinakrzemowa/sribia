699629bd08e228cfea4fa1533ddc8f617fe4bf6b
Bartosz Kalinowski <kelostrada@gmail.com>
2023-07-26 00:16:08 +0300

    add items sprites

--- lib/abyss/game/map_loader.ex
+++ lib/abyss/game/map_loader.ex
@@ -14,7 +14,6 @@ defmodule Abyss.Game.MapLoader do
 
       Logger.info("[MapLoader] Map file unzipped")
 
-
       map_file = File.read!("priv/map.json")
       Logger.info("[MapLoader] Loaded Map file")
 
@@ -25,18 +24,29 @@ defmodule Abyss.Game.MapLoader do
 
       Logger.info("[MapLoader] Total features: #{Enum.count(map_node["features"])}")
 
+      Logger.info("[MapLoader] Loading objects...")
+
+      items_file = File.read!("priv/items.json")
+      Logger.info("[MapLoader] Loaded Items file")
+
+      {:ok, items} = Jason.decode(items_file)
+      Logger.info("[MapLoader] Decoded Items file")
+
       Enum.each(map_node["features"], fn feature ->
         if feature["type"] == 4 && feature["x"] >= 32000 && feature["y"] >= 32000 && feature["x"] <= 33000 && feature["y"] <= 33000 && feature["z"] == 7 do
           Enum.each(feature["tiles"], fn tile ->
             Cachex.put!(:map, {feature["x"] + tile["x"], feature["y"] + tile["y"], feature["z"]}, %{
               id: tile["tileid"],
-              items: tile["items"]
+              items: tile["items"],
+              details: Map.get(items, to_string(tile["tileid"]))
             })
           end)
         end
       end)
 
       Logger.info("[MapLoader] Finished loading Map")
+
+      # TODO: put items on Board
     end)
   end
 end
