9298705a57595f2b4dc47c0a11edc6ed18bf0b84
kelostrada <kelostrada@gmail.com>
2026-04-28 13:29:34 +0200

    fix: opentelemetry_phoenix adapter is cowboy2, not bandit

--- lib/abyss/application.ex
+++ lib/abyss/application.ex
@@ -4,7 +4,7 @@ defmodule Abyss.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    OpentelemetryPhoenix.setup(adapter: :bandit)
+    OpentelemetryPhoenix.setup(adapter: :cowboy2)
     OpentelemetryEcto.setup([:abyss, :repo])
 
     # Define workers and child supervisors to be supervised
